f5d7e031d93f0f2693d835f523c053a73ca8add1
Robert Nubel <rnubel@enovafinancial.com>
2011-12-12 16:16:34 -0600

    Creates a mock world before starting the game.

--- game.rb
+++ game.rb
@@ -8,11 +8,12 @@ end
 
 # Create the world.
 world = World.new
+room = Room.new :description => "You stand in the middle of nowhere."
+world.current_room = room
 
 # Loop on user's commands.
 while true do
   command_str = ask("<%= color('What do you do?', BOLD + GREEN)%>")
   command = CommandParser.new(command_str).get_command
   command.execute(world)
-
 end
