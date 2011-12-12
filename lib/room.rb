4e3fbee49da29f2133820db1bf6e85c172e46928
Robert Nubel <rnubel@enovafinancial.com>
2011-12-12 16:06:05 -0600

    Fleshes out Room with a basic description.

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,6 @@
 class Room
-
+  attr_accessor :description
+  def initialize(args = {})
+    self.description = args[:description] || ""
+  end
 end
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,7 +1,13 @@
 require 'spec/spec_helper'
 
 describe Room do
-  it "should instantiate" do
-    Room.new.should_not be_nil
+  it "should instantiate with arguments" do
+    Room.new({}).should_not be_nil
+  end
+
+  it "should have a description" do
+    r = Room.new(:description => "Test")
+
+    r.description.should == "Test"
   end
 end
