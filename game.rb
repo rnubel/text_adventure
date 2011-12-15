4e151b65d269e2ca17f983de26336ad673726fd9
Robert Nubel <rnubel@enovafinancial.com>
2011-12-15 15:35:31 -0600

    Updates Game to reflect recent changes.

--- game.rb
+++ game.rb
@@ -11,8 +11,8 @@ end
 world = World.new
 room = Room.new :description => "You stand in the middle of nowhere.", 
                 :occupants => [ Actor.new("Bob") ]
-world.current_player = Actor.new("Player")
-world.current_player.current_room = room
+world.player = Actor.new("Player")
+world.player.current_room = room
 
 # Loop on user's commands.
 while true do
