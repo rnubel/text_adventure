5d6e07584cb2616a1c5f17df919b3050036679f1
Robert Nubel <robert.nubel@gmail.com>
2011-12-18 01:31:33 -0600

    Refactors Event; adds Event::Murder.

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,15 @@
-class Event
+module Event
+  module Base
 
+  end
+
+  class Murder
+    include Base
+
+    attr_accessor :originator, :target
+    def initialize(args = {})
+      @originator  = args[:originator]
+      @target      = args[:target]
+    end
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,7 +1,13 @@
 require 'spec/spec_helper'
 
-describe Event do
-  it "should instantiate" do
+describe Event::Murder do
+  it "should instantiate with a originator and target" do
+    a1 = mock()
+    a2 = mock()
+    e = Event::Murder.new(:originator => a1,
+                          :target     => a2)
 
+    e.originator.should == a1
+    e.target.should == a2
   end
 end
